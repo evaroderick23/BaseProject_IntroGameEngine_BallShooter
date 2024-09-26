f58945397a9c8964a146e07d9da9f63a3afed510
evaroderick23 <evaroderick23@gmail.com>
2024-09-25 21:51:39 -0300

    finished up levels 2 and 3

---
+++ Assets/Scripts/Moving Objects/ConeMove.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ConeMove : MonoBehaviour
+{
+    [SerializeField] float distanceToCover;
+    [SerializeField] float speed;
+
+    private Vector3 startingPosition;
+
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 v = startingPosition;
+        v.x += distanceToCover * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+}
