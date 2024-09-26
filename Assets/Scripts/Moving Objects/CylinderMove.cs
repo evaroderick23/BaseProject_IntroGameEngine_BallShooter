c06f7ca94b2d9f49a4f55ef786280e9b639b570c
evaroderick23 <evaroderick23@gmail.com>
2024-09-25 22:39:15 -0300

    started level 4

---
+++ Assets/Scripts/Moving Objects/CylinderMove.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CylinderMove : MonoBehaviour
+{
+    [SerializeField] float distanceToCover;
+    [SerializeField] float speed;
+
+    private Vector3 startingPosition;
+
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 v = startingPosition;
+        v.x += distanceToCover * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+}
